c4544c6c17d47edcdae2667f1743a0d78eb739d3
Luiz Roberto <luizrobertoao29@gmail.com>
2023-03-23 13:46:22 -0300

    First commit.

---
+++ src/main/java/com/mymovies/mymoviesapp/MyMoviesAppApplication.java
@@ -0,0 +1,15 @@
+package com.mymovies.mymoviesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class MyMoviesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymovies/mymoviesapp/MyMoviesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymovies.mymoviesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
