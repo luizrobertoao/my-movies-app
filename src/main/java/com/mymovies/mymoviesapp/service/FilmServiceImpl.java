bdac3523e54dba14f4b0d2cea8202b4381abb44b
Luiz Roberto <luizrobertoao29@gmail.com>
2023-03-24 10:44:31 -0300

    fix: Fixes annotation.

--- src/main/java/com/mymovies/mymoviesapp/service/FilmServiceImpl.java
+++ src/main/java/com/mymovies/mymoviesapp/service/FilmServiceImpl.java
@@ -3,9 +3,11 @@ package com.mymovies.mymoviesapp.service;
 import com.mymovies.mymoviesapp.model.Film;
 import com.mymovies.mymoviesapp.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FilmServiceImpl implements IFilmService {
 
     @Autowired
