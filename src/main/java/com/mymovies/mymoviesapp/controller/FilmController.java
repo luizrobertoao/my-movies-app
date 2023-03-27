199181404451da0054d4e3616103884fd1e58326
Luiz Roberto <luizrobertoao29@gmail.com>
2023-03-27 17:30:39 -0300

    Adds name and description of the API to swagger-ui.

--- src/main/java/com/mymovies/mymoviesapp/controller/FilmController.java
+++ src/main/java/com/mymovies/mymoviesapp/controller/FilmController.java
@@ -3,13 +3,20 @@ package com.mymovies.mymoviesapp.controller;
 import com.mymovies.mymoviesapp.gateway.entity.Search;
 import com.mymovies.mymoviesapp.model.Film;
 import com.mymovies.mymoviesapp.service.FilmServiceImpl;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@OpenAPIDefinition(
+        info = @Info(
+                title = "My Movie App",
+                version = "v1",
+                description = "Your personal movie catalog."))
 public class FilmController {
 
     @Autowired
